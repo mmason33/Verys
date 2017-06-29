8b217d9af23d137135459a4a935b7731f331ccc4
Michael Mason <michael@michaeljmason.com>
2017-06-28 19:19:17 -0700

    comment typo & changes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //BlackJack
 
 /**
-* Create Game object
+* @constructor Game();
+* Create Game
 */
 function Game() {
 	/**
-	* @param {string[]} this.players players object
-	* Initialize players collection with the give properties
+	* @param {string[]} this.players players array
+	* Initialize players collection with the given properties
 	*/
 	this.players = [
 		{
@@ -41,12 +42,12 @@ function Game() {
 		[ 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 	];
 	/**
-	* Deal - For players in players(collection/array), generate two random cards.
-	* @param {number} suit A random number between 1 & this.deck.length(4)
-	* @param {number} card A random number between 1 & this.deck[suit].length(13)
+	* Deal - For each player in the players(collection/array), generate two random cards.
+	* @param {number} suit A random whole number between 1 & this.deck.length(4)
+	* @param {number} card A random whole number between 1 & this.deck[suit].length(13)
 	* @example
 	* game.deal();
-	* console.log( players[player].cards, players[player].total ); // [9, 6], 15 [2, 5], 7 [10, 11], 21 [4, 11] 15 
+	* console.log( this.players[player].cards, this.players[player].total ); // [9, 6], 15 [2, 5], 7 [10, 11], 21 [4, 11] 15 
 	* Each random card value is assigned to this.players[player].total = this.players[player].total + this.deck[suit][card].
 	* After each random card selection, the value of that choice is removed from this.deck via splice(startIndex, deleteCount)
 	*/
@@ -64,12 +65,13 @@ function Game() {
 	}
 	/**
 	*
-	* Winner - determine the winner based on the output from game.deal();
+	* Winner - determine the winner based on the output of game.deal();
 	* @param {number} greatest The greatest sum of the dealt hands.
 	* @param {string} holder The name of the player with the greatest hand.
 	* @param {number} tie The value of the tying hand should there be a tie.
 	* @param {string} tieHolder The name of the player with the tying hand.
 	* @example
+	* game.winner();
 	* console.log(game.winner()); // The winner is Player 2 with a total of 20 || It's a tie between Player 1 and Dealer with a score of 21
 	*/
 	this.winner = () => {
@@ -101,7 +103,7 @@ function Game() {
 */
 game = new Game();
 /**
-* Invoke the this.deal() method on the new Game() Object assigned to game.
+* Invoke the this.deal() method on the new Game() assigned to game.
 */
 game.deal();
 /**
@@ -113,6 +115,6 @@ for(var player in game.players) {
 	console.log(game.players[player].total);
 }
 /**
-* Invoke the this.winner() method on the Game() Object to determine the winner & log the winner to the console.
+* Invoke the this.winner() method on the Game() to determine the winner & log the winner to the console.
 */
 console.log(game.winner());
